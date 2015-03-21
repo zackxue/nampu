b11d4aa5b90f2de0745deb0d08c14b4c06d2356c
camelscode <shiyong.zhang@qq.com>
2015-03-22 00:14:07 +0800

    Changes to be committed: 	deleted:    COPYING 	deleted:    INSTALL 	deleted:    Makefile.am 	deleted:    ar-lib 	deleted:    autogen.sh 	deleted:    compile 	deleted:    config.h 	deleted:    config.h.in 	deleted:    configure 	deleted:    configure.ac 	deleted:    install-sh 	deleted:    libtool 	deleted:    ltmain.sh 	deleted:    set_env.sh 	deleted:    src/core/Makefile.am 	deleted:    src/core/Makfile.deps 	deleted:    src/core/O_TARG 	deleted:    src/core/ctl/.dirstamp 	deleted:    src/core/ctl/nmp_args.c 	deleted:    src/core/ctl/nmp_sysctl.c 	deleted:    src/core/msg/.dirstamp 	deleted:    src/core/msg/nmp_message.c 	deleted:    src/lib/L_TARG 	deleted:    src/lib/Makefile 	deleted:    src/lib/bin/libdbc.a 	modified:   src/lib/bin/libencrypt.a 	deleted:    src/lib/bin/libjpfsearch.a 	modified:   src/lib/bin/libprocessxml.a 	deleted:    src/lib/bin/libtw.a 	deleted:    src/lib/db_connect_pool/Makefile.am 	modified:   src/lib/db_connect_pool/Makefile.deps 	deleted:    src/lib/db_connect_pool/src/db_connect_pool.c 	deleted:    src/lib/encrypt/Makefile.am 	modified:   src/lib/encrypt/Makefile.deps 	deleted:    src/lib/encrypt/inc/ET199.h 	deleted:    src/lib/encrypt/inc/ET199_32.h 	deleted:    src/lib/encrypt/inc/PET199_32.h 	deleted:    src/lib/encrypt/inc/encrypt.h 	deleted:    src/lib/encrypt/inc/fake.h 	deleted:    src/lib/encrypt/libET199.a 	modified:   src/lib/process_xml/Makefile 	deleted:    src/lib/process_xml/Makefile.am 	modified:   src/lib/process_xml/Makefile.deps 	deleted:    src/lib/process_xml/Makefile.in 	deleted:    src/lib/process_xml/src/Makefile.am 	modified:   src/lib/search_device/Makefile 	deleted:    src/lib/search_device/Makefile.am 	modified:   src/lib/search_device/Makefile.deps 	deleted:    src/lib/search_device/Makefile.in 	deleted:    src/lib/search_device/src/Makefile.am 	deleted:    src/lib/search_device/src/list.h 	deleted:    src/lib/search_device/src/main.c 	deleted:    src/lib/search_device/src/mb_api.c 	deleted:    src/lib/search_device/src/mb_api.h 	deleted:    src/lib/search_device/src/mb_basic.c 	deleted:    src/lib/search_device/src/mb_basic.h 	deleted:    src/lib/search_device/src/mb_cu.c 	deleted:    src/lib/search_device/src/mb_cu.h 	deleted:    src/lib/search_device/src/mb_pu.c 	deleted:    src/lib/search_device/src/mb_pu.h 	deleted:    src/lib/search_device/src/search_device.c 	modified:   src/lib/tvwall/Makefile 	deleted:    src/lib/tvwall/Makefile.am 	modified:   src/lib/tvwall/Makefile.deps 	deleted:    src/lib/tvwall/Makefile.in 	deleted:    src/lib/tvwall/include/nmp_tv_wall.h 	deleted:    src/lib/tvwall/src/Makefile.am 	deleted:    src/lib/tvwall/src/nmp_tv_wall.c 	modified:   src/mod/Makefile 	deleted:    src/mod/Makefile.am 	deleted:    src/mod/Makefile.in 	deleted:    src/mod/Makfile.deps 	deleted:    src/mod/O_TARG 	modified:   src/mod/shared/nmp_mods.o 	modified:   src/mod/shared/nmp_res_ctl.o 	modified:   src/mod/shared/nmp_shared.o 	modified:   src/mod/shared/nmp_utility.o 	modified:   src/mod/shared/nmp_xml_interface.o 	modified:   src/proto/Makefile 	deleted:    src/proto/Makefile.am 	deleted:    src/proto/Makefile.in 	deleted:    src/proto/Makfile.deps 	deleted:    src/proto/O_TARG

--- nmp-cms/set_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/root/platform/jpf/sbin/mysql/mysql-5.1.49/lib/mysql/
-export CMS_CONFIG_PATH=/root/platform/jpf/etc
-export PATH=/root/platform/jpf/sbin/mysql:$PATH:/home/zyt/zhengyt/sh/

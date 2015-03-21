6c82bc8372e923984d9befd5d8a6b51a05d4e51e
camelscode <shiyong.zhang@qq.com>
2015-03-22 00:16:58 +0800

    Changes to be committed: 	new file:   core/Makefile 	new file:   core/nmp_args.c 	new file:   core/nmp_message.c 	new file:   core/nmp_sysctl.c 	deleted:    lib/bin/libencrypt.a 	deleted:    lib/bin/libprocessxml.a 	new file:   lib/db_connect_pool/Makefile 	deleted:    lib/db_connect_pool/Makefile.deps 	new file:   lib/db_connect_pool/db_connect_pool.c 	new file:   lib/encrypt/Makefile 	deleted:    lib/encrypt/Makefile.deps 	new file:   lib/encrypt/include/ET199.h 	new file:   lib/encrypt/include/ET199_32.h 	new file:   lib/encrypt/include/PET199_32.h 	new file:   lib/encrypt/include/encrypt.h 	new file:   lib/encrypt/include/fake.h 	new file:   lib/encrypt/src/libET199.a 	deleted:    lib/process_xml/Makefile.deps 	deleted:    lib/search_device/Makefile.deps 	new file:   lib/search_device/list.h 	new file:   lib/search_device/main.c 	new file:   lib/search_device/mb_api.c 	new file:   lib/search_device/mb_api.h 	new file:   lib/search_device/mb_basic.c 	new file:   lib/search_device/mb_basic.h 	new file:   lib/search_device/mb_cu.c 	new file:   lib/search_device/mb_cu.h 	new file:   lib/search_device/mb_pu.c 	new file:   lib/search_device/mb_pu.h 	new file:   lib/search_device/search_device.c 	deleted:    lib/tvwall/Makefile.deps 	new file:   lib/tvwall/nmp_tv_wall.c 	new file:   lib/tvwall/nmp_tv_wall.h 	deleted:    mod/shared/nmp_mods.o 	deleted:    mod/shared/nmp_res_ctl.o 	deleted:    mod/shared/nmp_shared.o 	deleted:    mod/shared/nmp_utility.o 	deleted:    mod/shared/nmp_xml_interface.o

---
+++ nmp-cms/src/core/nmp_args.c
@@ -0,0 +1,103 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "nmp_args.h"
+#include "nmp_version.h"
+
+void jpf_print_help_manual()
+{
+	const char* p_manual_msg =
+	"CMS(Center Management Server) Help Manual.\n"
+	"\n"
+	"\t-H, --help\n"
+	"\t\tPrint this help, then exit.\n"
+	"\n"
+	"\t-h\n"
+	"\t\tPrint usage, then exit.\n"
+	"\n"
+	"\t-v, --version\n"
+	"\t\tPrint cms version number, then exit.\n"
+	"\n"
+	"The end\n";
+	printf("%s", p_manual_msg);
+}
+
+void jpf_print_usage()
+{
+	const char* p_usage_msg =
+	"Usage: cms [-hvH]\n"
+	"Try \"cms --help\" to see more details!\n";
+	printf("%s", p_usage_msg);
+}
+
+void jpf_print_version()
+{
+	printf(
+		"Center Management Server(CMS)\n"
+		"Version %0x  %d.%d.%d.%d  "
+		"%s %s\n",
+		__CMS_VERSION__,
+		((__CMS_VERSION__)>>24)&0xff,
+		((__CMS_VERSION__)>>16)&0xff,
+		((__CMS_VERSION__)>>8)&0xff,
+		(__CMS_VERSION__)&0xff,
+		__DATE__,
+		__TIME__
+	);
+}
+
+static struct option optlongs[] =
+{
+	{"help", 0, NULL, 'H'},
+	{"version", 0, NULL, 'v'},
+	{NULL, 0, NULL, 'h'},
+	{NULL, 0, 0, 0}
+};
+
+
+
+void jpf_process_main_args(int argc, char* argv[])
+{
+	int opt;
+
+	while((opt = getopt_long(argc, argv, "hHv",
+		optlongs, NULL)) != -1)
+	{
+		switch(opt)
+		{
+			case 'h':
+			{
+				jpf_print_usage();
+				exit(0);
+			}
+			case 'H':
+			{
+				jpf_print_help_manual();
+				exit(0);
+			}
+			case 'v':
+			{
+				jpf_print_version();
+				exit(0);
+			}
+
+			case ':':
+			case '?':
+			{
+				jpf_print_usage();
+				exit(1);
+			}
+		}//switch
+	}//while
+}
+
+
+
+
+
+
+
+
+
+
+
